808389b3cadf06b0590692a4d3a5e7d9015afbb0
sinkcup <sinkcup@163.com>
2016-01-20 15:41:11 +0800

    change page: connect action and store

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HomeAction from '../stores/HomeAction';
+import ApiStore from '../stores/ApiStore';
+import { connectToStores } from 'fluxible-addons-react';
 
+@connectToStores ([ApiStore], (context) => ({
+    'ApiStore': context.getStore(ApiStore).getData()
+}))
 class Home extends React.Component {
     render() {
+        console.log(this.props.ApiStore);
         return (
             <div>
                 <h2>Home</h2>
@@ -10,5 +17,7 @@ class Home extends React.Component {
         );
     }
 }
+
+HomeAction.getUsers;
 
 export default Home;
